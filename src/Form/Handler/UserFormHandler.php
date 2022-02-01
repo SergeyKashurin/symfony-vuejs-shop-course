36b1e6eb1cf082a55690cdad5271058791f981d0
Sergey Kashurin <sergey-kashurin@mail.ru>
2022-02-01 20:34:40 +0300

    Added saving user

--- src/Form/Handler/UserFormHandler.php
+++ src/Form/Handler/UserFormHandler.php
@@ -36,9 +36,14 @@ class UserFormHandler
     public function processEditForm(Form $form)
     {
         $plainPassword = $form->get('plainPassword')->getData();
+        $newEmail = $form->get('newEmail')->getData();
 
         /** @var User $user */
         $user = $form->getData();
+
+        if(!$user->getId()) {
+            $user->setEmail($newEmail);
+        }
 
         if($plainPassword) {
             $encodedPassword = $this->passwordEncoder->encodePassword($user, $plainPassword);
