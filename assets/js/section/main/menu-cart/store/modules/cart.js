a2bfd249518b13cade3ccdc02d923ca1a06a37c7
Sergey Kashurin <sergey-kashurin@mail.ru>
2022-02-08 22:07:43 +0300

    Added total price for vue cart

--- assets/js/section/main/menu-cart/store/modules/cart.js
+++ assets/js/section/main/menu-cart/store/modules/cart.js
@@ -5,23 +5,88 @@ import {concatUrlByParams} from "../../../../../utils/url-generator";
 
 
 const state = () => ({
+    cart: {},
 
     staticStore: {
-
+        url: {
+            apiCart:            window.staticStore.urlCart,
+            apiCartProduct:     window.staticStore.urlCartProduct,
+            viewProduct:        window.staticStore.urlViewProduct,
+            viewCart:           window.staticStore.urlViewCart,
+            assetImageProducts: window.staticStore.urlAssetImageProducts,
+        },
     },
 });
 
 const getters = {
+    totalPrice(state) {
+        let result = 0;
+        console.log(state.cart);
+        if(!state.cart.cartProducts) {
+            return 0;
+        }
 
+        state.cart.cartProducts.forEach(
+            cartProduct => {
+                result += cartProduct.product.price * cartProduct.quantity
+            }
+        )
+
+        return result;
+    },
 };
 
 
 const actions = {
+    async getCart({ state, commit }) {
+        const url = state.staticStore.url.apiCart;
 
+        const result = await axios.get(url, apiConfig);
+
+        if(
+            result.data
+            && result.data["hydra:member"].length
+            && result.status === StatusCodes.OK
+        ) {
+            commit('setCart', result.data["hydra:member"][0]);
+        } else {
+            // commit('setAlert', {
+            //     type: 'info',
+            //     message: 'Your cart is empty ...',
+            // });
+        }
+    },
+    async cleanCart({ state, commit }) {
+        const url = concatUrlByParams(
+            state.staticStore.url.apiCart
+            + '/'
+            + state.cart.id
+        );
+
+        const result = await axios.delete(url, apiConfig);
+
+        if(result.status === StatusCodes.NO_CONTENT) {
+            commit('setCart', {});
+        }
+    },
+    async removeCartProduct({ state, commit, dispatch }, cartProductId) {
+        const url = concatUrlByParams(
+            state.staticStore.url.apiCartProduct,
+            cartProductId
+        );
+
+        const result = await axios.delete(url, apiConfig);
+
+        if(result.status === StatusCodes.NO_CONTENT) {
+            dispatch('getCart');
+        }
+    },
 };
 
 const mutations = {
-
+    setCart(state, cart) {
+        state.cart = cart;
+    },
 };
 
 export default {
