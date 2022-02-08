a4b6917cf43281c8a108650c2b32bc4be59b7afa
Sergey Kashurin <sergey-kashurin@mail.ru>
2022-02-08 22:29:38 +0300

    Added products list for vue cart

--- assets/js/section/main/menu-cart/components/CartProductList.vue
+++ assets/js/section/main/menu-cart/components/CartProductList.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>test component list</div>
+  <div class="products">
+    <CartProductItem
+      v-for="cartProduct in cart.cartProducts"
+      :cart-product="cartProduct"
+      :key="cartProduct.id"
+    />
+  </div>
 </template>
 
 <script>
+import CartProductItem from "./CartProductItem";
+import {mapState} from "vuex";
 export default {
-  name: "CartProductList"
+  name: "CartProductList",
+  components: {CartProductItem},
+  computed: {
+    ...mapState("cart", ["cart"]),
+  },
 }
 </script>
 
