e13eb4d43b6a9d64732be31a148f6008be201e01
Sergey Kashurin <sergey-kashurin@mail.ru>
2022-02-10 22:19:02 +0300

    Created recovery password and some fix for auth via Facebook and Google

--- src/Command/AddUserCommand.php
+++ src/Command/AddUserCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
@@ -13,23 +14,28 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\Stopwatch\Stopwatch;
 
 class AddUserCommand extends Command
 {
     protected static $defaultName = 'app:add-user';
     protected static $defaultDescription = 'Create user';
-    private EntityManagerInterface $entityManager;
-    private UserPasswordEncoderInterface $encoder;
-    private UserRepository $userRepository;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
 
-    public function __construct(
-        string $name = null,
-        EntityManagerInterface $entityManager,
-        UserPasswordEncoderInterface $encoder,
-        UserRepository $userRepository
-    )
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(string $name = null, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder, UserRepository $userRepository)
     {
         parent::__construct($name);
         $this->entityManager = $entityManager;
@@ -41,83 +47,60 @@ class AddUserCommand extends Command
     {
         $this
             ->setDescription(self::$defaultDescription)
-            ->addOption('email', 'em', InputArgument::REQUIRED, 'Email')
-            ->addOption('password', 'p', InputArgument::REQUIRED, 'Password')
-            ->addOption('isAdmin', '', InputArgument::OPTIONAL, 'If set the user is 
-                created as an administrator', 0)
+            ->addOption('email', 'em', InputArgument::OPTIONAL, 'Email')
+            ->addOption('password', 'p', InputArgument::OPTIONAL, 'Password')
+            ->addOption('role', '', InputArgument::OPTIONAL, 'Set role')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-
         $stopwatch = new Stopwatch();
-        $stopwatch->start('app-user-command');
+        $stopwatch->start('add-user-command');
 
         $email = $input->getOption('email');
         $password = $input->getOption('password');
-        $isAdmin = $input->getOption('isAdmin');
+        $role = $input->getOption('role');
 
         $io->title('Add User Command Wizard');
         $io->text([
-            'Please enter some information!'
+            'Please, enter some information'
         ]);
 
-        if(!$email) {
+        if (!$email) {
             $email = $io->ask('Email');
         }
 
-        if(!$password) {
+        if (!$password) {
             $password = $io->askHidden('Password (your type will be hidden)');
         }
 
-        if(!$isAdmin) {
-            $question = new Question("Is admin (1 or 0)", 0);
-            $isAdmin = $io->askQuestion($question);
+        if (!$role) {
+            $role = $io->ask('Set role');
         }
 
-        $isAdmin = boolval($isAdmin);
-
         try {
-            $user = $this->createUser($email, $password, $isAdmin);
+            $user = $this->createUser($email, $password, $role);
         } catch (RuntimeException $exception) {
             $io->comment($exception->getMessage());
 
             return Command::FAILURE;
         }
 
-
-
-        $successMassage = sprintf('%s was successfully created: %s',
-            $isAdmin ? 'Administrator user' : 'User',
+        $successMessage = sprintf('%s was successfully created: %s',
+            $role,
             $email
         );
-        $io->success($successMassage);
+        $io->success($successMessage);
 
-        $event = $stopwatch->stop('app-user-command');
+        $event = $stopwatch->stop('add-user-command');
         $stopwatchMessage = sprintf('New user\'s id: %s / Elapsed time: %.2f ms / Consumed memory: %.2f MB',
             $user->getId(),
             $event->getDuration(),
             $event->getMemory() / 1000 / 1000
         );
-
         $io->comment($stopwatchMessage);
-
-        /*
-        dd($email, $password, $isAdmin);
-        if ($email) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
-        */
-
-
 
         return Command::SUCCESS;
     }
@@ -125,29 +108,24 @@ class AddUserCommand extends Command
     /**
      * @param string $email
      * @param string $password
-     * @param bool $isAdmin
+     * @param string $role
      * @return User
      */
-    private function createUser(string $email, string $password, bool $isAdmin): \App\Entity\User
+    private function createUser(string $email, string $password, string $role): User
     {
         $existingUser = $this->userRepository->findOneBy(['email' => $email]);
 
-        if($existingUser) {
+        if ($existingUser) {
             throw new RuntimeException('User already exist');
         }
 
-        $user = new \App\Entity\User();
+        $user = new User();
         $user->setEmail($email);
+        $user->setRoles([$role]);
 
-        $role[] = 'ROLE_USER';
-        if($isAdmin ? 'ROLE_ADMIN' : 'ROLE_USER') {
-            $role[] = 'ROLE_ADMIN';
-        }
+        $encodedPassword = $this->encoder->encodePassword($user, $password);
+        $user->setPassword($encodedPassword);
 
-        $user->setRoles($role);
-
-        $encoderPassword = $this->encoder->encodePassword($user, $password);
-        $user->setPassword($encoderPassword);
         $user->setIsVerified(true);
 
         $this->entityManager->persist($user);
