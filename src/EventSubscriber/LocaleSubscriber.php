b92c47f76d081e79938317eda4b57dbf177a49a6
Sergey Kashurin <sergey-kashurin@mail.ru>
2022-02-11 11:32:32 +0300

    Added support en|fr|ru languages to project and configure it with locale subscriber and yaml's conf

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+
+    /**
+     * @var string
+     */
+    private string $defaultLocale;
+
+    /**
+     * @param string $defaultLocale
+     */
+    public function __construct(string $defaultLocale)
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    /**
+     * @param RequestEvent $event
+     */
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+        if(!$request->hasPreviousSession()) {
+            return;
+        }
+
+        $locale = $request->attributes->get('_locale');
+        if($locale) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            $request->getSession()->get('_locale', $this->defaultLocale);
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => [
+                [
+                    'onKernelRequest', 20
+                ]
+            ]
+        ];
+    }
+}
