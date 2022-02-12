5cf73fe422dfac06699fdfc2fd7b3d17c701e986
Sergey Kashurin <sergey-kashurin@mail.ru>
2022-02-12 13:04:49 +0300

    Configure knp bundle, and order controller, form and handler

--- src/Controller/Admin/OrderController.php
+++ src/Controller/Admin/OrderController.php
@@ -3,11 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Order;
-use App\Entity\Category;
 use App\Entity\OrderProduct;
 use App\Entity\StaticStorage\OrderStaticStorage;
 use App\Form\Admin\EditOrderFormType;
-use App\Form\Handler\OrderFormHandled;
+use App\Form\Admin\FilterType\OrderFilterFormType;
+use App\Form\DTO\EditOrderModel;
+use App\Form\Handler\OrderFormHandler;
 use App\Repository\OrderRepository;
 use App\Utils\Manager\OrderManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,89 +22,59 @@ use Symfony\Component\Routing\Annotation\Route;
 class OrderController extends AbstractController
 {
     /**
-     * @return Response
-     *
      * @Route("/list", name="list")
      */
-    public function list(OrderRepository $orderRepository): Response
+    public function list(Request $request, OrderFormHandler $orderFormHandler): Response
     {
+        $model = new EditOrderModel();
+        $filterForm = $this->createForm(OrderFilterFormType::class, $model);
+        $filterForm->handleRequest($request);
 
-        $orders = $orderRepository->findBy(['isDeleted' => false], ['id' => 'DESC']);
+        $pagination = $orderFormHandler->processOrderFiltersForm($request, $filterForm);
+
         return $this->render('admin/order/list.html.twig', [
-            'orders' => $orders,
-            'orderStatusChoices' => OrderStaticStorage::getOrderStatusChoices()
+            'pagination' => $pagination,
+            'orderStatusChoices' => OrderStaticStorage::getOrderStatusChoices(),
+            'form' => $filterForm->createView()
         ]);
     }
 
-
     /**
-     * @param Request $request
-     * @param Category|null $category
-     * @return Response
-     *
-     * @Route("/edit/{id}", name="edit", requirements={"id"="\d+"})
+     * @Route("/edit/{id}", name="edit")
      * @Route("/add", name="add")
      */
-    public function edit(Request $request, OrderFormHandled $orderFormHandled, Order $order= null): Response
+    public function edit(Request $request, OrderFormHandler $orderFormHandler, Order $order = null): Response
     {
-
-        if(!$order) {
+        if (!$order) {
             $order = new Order();
         }
 
         $form = $this->createForm(EditOrderFormType::class, $order);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
-            $order = $orderFormHandled->processEditForm($order);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $order = $orderFormHandler->processEditForm($order);
 
             $this->addFlash('success', 'Your changes were saved!');
 
-            return $this->redirectToRoute('admin_order_edit', [
-                'id' => $order->getId(),
-            ]);
+            return $this->redirectToRoute('admin_order_edit', ['id' => $order->getId()]);
         }
 
-        if($form->isSubmitted() && !$form->isValid())
-        {
-            $this->addFlash('warning', 'Something went wrong. Please check your form.');
+        if ($form->isSubmitted() && !$form->isValid()) {
+            $this->addFlash('warning', 'Something went wrong. Please check your form!');
         }
 
         $orderProducts = [];
-        /**
-         * @var OrderProduct $product
-         */
-//        foreach ($order->getOrderProducts()->getValues() as $product) {
-//            $orderProducts[] = [
-//                'id' => $product->getId(),
-//                'product' => [
-//                    'id' => $product->getProduct()->getId(),
-//                    'title' => $product->getProduct()->getTitle(),
-//                    'price' => $product->getProduct()->getPrice(),
-//                    'quantity' => $product->getProduct()->getQuantity(),
-//                    'category' => [
-//                        'id' => $product->getProduct()->getCategory()->getId(),
-//                        'title' => $product->getProduct()->getCategory()->getTitle(),
-//                    ],
-//                ],
-//                'quantity' => $product->getProduct()->getQuantity(),
-//                'pricePerOne' => $product->getPricePerOne(),
-//            ];
-//        }
 
         return $this->render('admin/order/edit.html.twig', [
             'order' => $order,
             'orderProducts' => $orderProducts,
-            'form' => $form->createView(),
+            'form' => $form->createView()
         ]);
     }
 
     /**
-     * @param Order $order
-     * @param OrderManager $orderManager
-     * @return Response
-     *
-     * @Route("/delete/{id}", name="delete", requirements={"id"="\d+"})
+     * @Route("/delete/{id}", name="delete")
      */
     public function delete(Order $order, OrderManager $orderManager): Response
     {
---
+++ src/Form/Handler/OrderFormHandler.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form\Handler;
+
+use App\Entity\Order;
+use App\Utils\Manager\OrderManager;
+use Knp\Component\Pager\PaginatorInterface;
+use Lexik\Bundle\FormFilterBundle\Filter\FilterBuilderUpdater;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class OrderFormHandler
+{
+    /**
+     * @var OrderManager
+     */
+    private $orderManager;
+    /**
+     * @var PaginatorInterface
+     */
+    private $paginator;
+    /**
+     * @var FilterBuilderUpdater
+     */
+    private $filterBuilderUpdater;
+
+    /**
+     * @param OrderManager $orderManager
+     * @param PaginatorInterface $paginator
+     * @param FilterBuilderUpdater $filterBuilderUpdater
+     */
+    public function __construct(OrderManager $orderManager, PaginatorInterface $paginator, FilterBuilderUpdater $filterBuilderUpdater)
+    {
+        $this->orderManager = $orderManager;
+        $this->paginator = $paginator;
+        $this->filterBuilderUpdater = $filterBuilderUpdater;
+    }
+
+    /**
+     * @param Request $request
+     * @param FormInterface $filterForm
+     * @return \Knp\Component\Pager\Pagination\PaginationInterface
+     */
+    public function processOrderFiltersForm(Request $request, FormInterface $filterForm)
+    {
+        $queryBuilder = $this->orderManager->getRepository()
+            ->createQueryBuilder('o')
+            ->leftJoin('o.owner', 'u')
+            ->where('o.isDeleted = :isDeleted')
+            ->setParameter('isDeleted', false);
+
+        if ($filterForm->isSubmitted()) {
+            $this->filterBuilderUpdater->addFilterConditions($filterForm, $queryBuilder);
+        }
+
+        return $this->paginator->paginate(
+            $queryBuilder->getQuery(),
+            $request->query->getInt('page', 1)
+        );
+    }
+
+    /**
+     * @param Order $order
+     * @return Order|null
+     */
+    public function processEditForm(Order $order)
+    {
+        $this->orderManager->recalculateOrderTotalPrice($order);
+        $this->orderManager->save($order);
+
+        return $order;
+    }
+}
